438ed87c2cecc2c7ecd09068a3d554de2e5d55b0
kyoheichachacha <n.kyohei@ksw-inc.co.jp>
2025-07-27 15:44:49 +0900

    Update script.js
    コメントアウト追加

--- script.js
+++ script.js
@@ -51,23 +51,28 @@
         },
         // スライドが切り替わり始めた直後に呼ばれるイベント
         slideChangeTransitionStart: function () {
-            // 
+            // 直前のスライドのインデックス番号を取得
             const prevIndex = this.previousIndex;
+            // 今回表示されるスライドのインデックス番号を取得
             const nextIndex = this.activeIndex;
+            // スライド幅にインデックス番号をかけて、前のスライドの左端の位置を計算
             const currentOffset = -prevIndex * this.width;
+            // 上と同様に、次のスライドの左端の位置を計算
             const fullOffset = -nextIndex * this.width;
+            // 前スライドと次スライドの20％の地点を計算
             const midOffset = currentOffset + (fullOffset - currentOffset) * 0.2;
             
-            // ズームアニメ適用 (全スライドクラス削除→次へ付与)
+            // 次に表示されるスライド自体の要素を取得
             const nextSlide = this.slides[this.activeIndex];
+            // 次スライドに.add-animationクラスを追加
             nextSlide.classList.add('add-animation');
             
-            // 最初の10%をゆっくり動かす
+            // 700msかけて20％までスライドを移動
                 this.setTransition(700); // 0.4秒
                 this.setTranslate(midOffset);
             
             setTimeout(() => {
-                // 残り90%をゆっくりに動かす
+                // 残り80%のスライドを400msで移動
                 this.setTransition(400); // 0.5秒
                 this.setTranslate(fullOffset);
             }, 400);
@@ -75,25 +80,32 @@
         },
     },
 });
-
+// 画面サイズに応じてSwiperのスライド方向を動的に切り替える関数
 function updateSwiperDirection() {
+    // 現在のウインドウ幅を取得
     const w = window.innerWidth;
+    // 横幅が969px〜561pxなら右から左にスライド、それ以外は左から右にスライド
     const isRTL = (w >= 561 && w <= 959) ? true : false;
+    //　Swiperが現在右から左にスライドしているかどうかを取得
     const currentDir = swiper.rtlTranslate;
-  
+    // 必要な時だけSwiperの言語向き・スライド向きを切り替える処理
     if (isRTL !== currentDir) {
-      swiper.changeLanguageDirection(isRTL ? 'rtl' : 'ltr'); // dir属性も切り替え
-      swiper.changeDirection('horizontal', false); // 方向は変わらないけど内部更新
+    　// Swiperも言語方向（内部のdir属性）の切り替え
+      swiper.changeLanguageDirection(isRTL ? 'rtl' : 'ltr'); 
+    　//　スライド方向は横のまま固定（rtlの設定変更時の再初期化のため）
+      swiper.changeDirection('horizontal', false); 
     }
   }
   
-  // 初期化と監視
+  // jQueryの「ページ読み込み完了」イベント
   $(function () {
-    initSwiper();
+    // 現在のウィンドウ幅に合わせてスライドの方向を設定
     updateSwiperDirection();
-  
+  　// 画面の幅が変わった時に実行されるイベント
     $(window).on('resize', function () {
+    　// 画面の幅を変えた時の処理を何回も実行しないためにタイマーの実行をキャンセルする関数
       clearTimeout(window._swiperResizeTimer);
+    　// 最後のリサイズ操作から300ms後に、updateSwiperDirection() を実行する処理
       window._swiperResizeTimer = setTimeout(updateSwiperDirection, 300);
     });
 });
